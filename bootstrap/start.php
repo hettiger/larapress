ad7492ad8b2b7b577441ca0dd253412adb80f6af
Martin Hettiger <Martin@Hettiger.com>
2014-04-28 01:10:53 +0200

    Add and update several configuration files Introduce .env configuration Add a dependency: barryvdh/laravel-ide-helper Update credits.md

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,13 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(
+    function () {
 
-	'local' => array('your-machine-name'),
+        return getenv('APP_ENV') ? : 'production';
 
-));
+    }
+);
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +43,7 @@ $env = $app->detectEnvironment(array(
 |
 */
 
-$app->bindInstallPaths(require __DIR__.'/paths.php');
+$app->bindInstallPaths(require __DIR__ . '/paths.php');
 
 /*
 |--------------------------------------------------------------------------
@@ -54,10 +56,10 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-$framework = $app['path.base'].
-                 '/vendor/laravel/framework/src';
+$framework = $app['path.base'] .
+    '/vendor/laravel/framework/src';
 
-require $framework.'/Illuminate/Foundation/start.php';
+require $framework . '/Illuminate/Foundation/start.php';
 
 /*
 |--------------------------------------------------------------------------
