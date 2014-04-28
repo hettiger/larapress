f02999aedef37558521ff2987c568edb1c1c82b5
Martin Hettiger <Martin@Hettiger.com>
2014-04-28 02:04:43 +0200

    Introduce the Larapress directory Add an empty install command

---
+++ app/Larapress/Commands/InstallCommand.php
@@ -0,0 +1,68 @@
+<?php namespace Larapress\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class InstallCommand extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'larapress:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install larapress.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return InstallCommand
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $this->info('This feature is still under development. Sorry!');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            array('example', InputArgument::OPTIONAL, 'An example argument.'),
+        );
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+        );
+    }
+
+}
