2309ee8ea6dc2cb113ef81f735dd44a634c86eb1
Martin Hettiger <Martin@Hettiger.com>
2014-04-28 20:30:46 +0200

    Add copyright notices to the login page Share the current time on all views

--- app/Larapress/Controllers/BaseController.php
+++ app/Larapress/Controllers/BaseController.php
@@ -1,6 +1,7 @@
 <?php namespace Larapress\Controllers;
 
 use App;
+use Carbon\Carbon;
 use Config;
 use Controller;
 use View;
@@ -12,9 +13,11 @@ class BaseController extends Controller
     {
         $lang = App::getLocale();
         $title = Config::get('larapress.names.cms') . ' | ' . trans('general.Login');
+        $now = Carbon::now();
 
         View::share('lang', $lang);
         View::share('title', $title);
+        View::share('now', $now);
     }
 
 }
--- app/views/larapress/layouts/login.blade.php
+++ app/views/larapress/layouts/login.blade.php
@@ -10,5 +10,9 @@
             <img src="{{ asset('larapress/assets/png/logo.png') }}">
             @yield('content')
         </div>
+        <p id="copyright">
+            &copy; {{{ $now->year }}} <a href="http://kontakt.hettiger.com" target="_blank">Martin Hettiger</a> |
+            Powered by <a href="http://laravel.com" target="_blank">Laravel</a>
+        </p>
     </div>
 @stop
