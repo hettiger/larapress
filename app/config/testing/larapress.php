9cb625e6e63009113d4a918921fd03c1f48d8347
Martin Hettiger <Martin@Hettiger.com>
2014-04-29 17:56:13 +0200

    Update the testing configuration and revert the filters test back

---
+++ app/config/testing/larapress.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings
+    |--------------------------------------------------------------------------
+    |
+    | Configure the CMS here
+    |
+    */
+
+    'settings' => array(
+
+        'ssl' => false, // Force to use https:// requests in the backend
+
+    ),
+
+);
