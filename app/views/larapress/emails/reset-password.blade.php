b4e22e31aea7cfd09a923f767372da0fc7ec88f7
Martin Hettiger <Martin@Hettiger.com>
2014-04-30 20:25:32 +0200

    Add translation for the reset password email view

--- app/views/larapress/emails/reset-password.blade.php
+++ app/views/larapress/emails/reset-password.blade.php
@@ -1,11 +1,14 @@
-Hello!
+@lang('email.Hello!')
 
-Someone, hopefully you, has requested a password reset for your {{{ $cms_name }}} cms account.
-By clicking on the following link you'll recceive a second email containing a new password:
+
+@lang('email.Someone, hopefully you, has requested a password reset for your ' . $cms_name . ' cms account.')
+
+@lang('email.By clicking on the following link you\'ll recceive a second email containing a new password:')
+
 
 {{ $url }}
 
 
-If you didn't request a password reset for your account, you can safely ignore this email.
+@lang('email.If you didn\'t request a password reset for your account, you can safely ignore this email.')
 
-Have a nice day!
+@lang('email.Have a nice day!')
