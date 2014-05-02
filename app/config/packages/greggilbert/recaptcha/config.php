bc30eeebb6411eb3bf6e8cf2fed00d4c4954c7ce
Martin Hettiger <Martin@Hettiger.com>
2014-05-02 14:07:00 +0200

    Update the reCAPTCHA config

--- app/config/packages/greggilbert/recaptcha/config.php
+++ app/config/packages/greggilbert/recaptcha/config.php
@@ -1,26 +1,47 @@
 <?php
 
+$app_lang = Config::get('app.locale');
+
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| API Keys
-	|--------------------------------------------------------------------------
-	|
-	| Set the public and private API keys as provided by reCAPTCHA.
-	|
-	*/
-	'public_key'	=> getenv('RECAPTCHA_PUBLIC'),
-	'private_key'	=> getenv('RECAPTCHA_PRIVATE'),
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Template
-	|--------------------------------------------------------------------------
-	|
-	| Set a template to use if you don't want to use the standard one.
-	|
-	*/
-	'template'		=> '',
-	
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    | Set the public and private API keys as provided by reCAPTCHA.
+    |
+    */
+    'public_key'  => getenv('RECAPTCHA_PUBLIC'),
+    'private_key' => getenv('RECAPTCHA_PRIVATE'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | Set a template to use if you don't want to use the standard one.
+    |
+    */
+    'template'    => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options
+    |--------------------------------------------------------------------------
+    |
+    | Apply all reCAPTCHA Options you may need here. A reference of available
+    | Options can be found at the google documentation:
+    |
+    | https://developers.google.com/recaptcha/docs/customization
+    |
+    */
+    'options'     => array(
+
+        'lang'  => $app_lang,
+
+        'theme' => 'white',
+
+    ),
+
 );
