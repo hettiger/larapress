65dd6a5e1aac7034efb23ccf084b7ba6ec599a1f
Martin Hettiger <Martin@Hettiger.com>
2014-05-08 08:43:31 +0200

    Test the force human filter

--- app/Larapress/Tests/Filters/FilterTest.php
+++ app/Larapress/Tests/Filters/FilterTest.php
@@ -1,5 +1,8 @@
 <?php namespace Larapress\Tests\Filters;
 
+use Captcha;
+use InvalidArgumentException;
+use Larapress\Filters\Special\ForceHumanFilter;
 use Larapress\Tests\TestCase;
 use Permission;
 use Route;
@@ -16,6 +19,53 @@ class FilterTest extends TestCase
         Route::enableFilters();
 
         $this->backend_route = Config::get('larapress.urls.backend');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | force.human Tests
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can test the force.human filter
+    |
+    */
+
+    public function test_can_return_null_if_captcha_is_not_required()
+    {
+        Captcha::shouldReceive('isRequired')->once()->andReturn(false);
+
+        $filter = new ForceHumanFilter;
+        $this->assertInternalType('null', $filter->filter());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Cannot redirect to an empty URL.
+     */
+    public function test_can_redirect_back_if_the_captcha_is_required()
+    {
+        Captcha::shouldReceive('isRequired')->once()->andReturn(true);
+
+        $filter = new ForceHumanFilter;
+        $filter->filter();
+    }
+
+    public function test_can_flash_an_error_message_to_the_session_if_the_captcha_is_required()
+    {
+        Captcha::shouldReceive('isRequired')->once()->andReturn(true);
+
+        $filter = new ForceHumanFilter;
+
+        try
+        {
+            $filter->filter();
+        }
+        catch (InvalidArgumentException $e)
+        {
+            $this->assertEquals('Cannot redirect to an empty URL.', $e->getMessage());
+        }
+
+        $this->assertSessionHas('error', 'Please verify that you are human first.');
     }
 
     /*
