8a5021e319189e43a878ce881322df238b1fd8d7
Martin Hettiger <Martin@Hettiger.com>
2014-05-09 15:07:46 +0200

    Update the 404 view so it doesn't look all that bad

--- app/views/larapress/errors/404.blade.php
+++ app/views/larapress/errors/404.blade.php
@@ -1,9 +1,24 @@
 @extends('larapress.layouts.default')
 
+@section('head.extension')
+    {{ HTML::style('larapress/assets/css/pages/errors/404.css') }}
+@stop
+
 @section('content')
-<center>
-    <h1>404 Error</h1>
-    <p>{{ HTML::link('/', 'Home') }}</p>
-    <p>{{ HTML::link('admin/login', 'Backend Login') }}</p>
-</center>
+    <div id="wrapper" class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="jumbotron">
+                <h1>@lang('general.404 Error')</h1>
+                <p>@lang('general.Sorry this page does not exist.')</p>
+                <p>
+                    {{ HTML::link('/', trans('general.Homepage'), array(
+                        'class' => 'btn btn-primary btn-lg',
+                        'role'  => 'button'
+                    )) }}
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
 @stop
