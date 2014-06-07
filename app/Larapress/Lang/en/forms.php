8beda29f3cd4d2d2c892dbbe68f61b708a5c593f
Martin Hettiger <Martin@Hettiger.com>
2014-06-07 16:47:01 +0200

    Reformat larapress language files

--- app/Larapress/Lang/en/forms.php
+++ app/Larapress/Lang/en/forms.php
@@ -2,20 +2,20 @@
 
 return array(
 
-    'Email address' => 'Email address',
+	'Email address' => 'Email address',
 
-    'Enter email' => 'Enter email',
+	'Enter email' => 'Enter email',
 
-    'Password' => 'Password',
+	'Password' => 'Password',
 
-    'Login' => 'Login',
+	'Login' => 'Login',
 
-    'Abort and go back' => 'Abort and go back',
+	'Abort and go back' => 'Abort and go back',
 
-    'Reset my password' => 'Reset my password',
+	'Reset my password' => 'Reset my password',
 
-    'Verify that you are human' => 'Verify that you are human',
+	'Verify that you are human' => 'Verify that you are human',
 
-    'Please verify that you are human first.' => 'Please verify that you are human first.',
+	'Please verify that you are human first.' => 'Please verify that you are human first.',
 
 );
--- app/Larapress/Lang/en/general.php
+++ app/Larapress/Lang/en/general.php
@@ -2,21 +2,21 @@
 
 return array(
 
-    'Login' => 'Login',
+	'Login' => 'Login',
 
-    'Dashboard' => 'Dashboard',
+	'Dashboard' => 'Dashboard',
 
-    '404 Error' => '404 Error',
+	'404 Error' => '404 Error',
 
-    'Click to reset your password' => 'Click to reset your password',
+	'Click to reset your password' => 'Click to reset your password',
 
-    'Reset Password' => 'Reset Password',
+	'Reset Password' => 'Reset Password',
 
-    'The control panel requires JavaScript support. Here are the <a href="http://www.enable-javascript.com/" target="_blank">instructions how to enable JavaScript in your web browser</a>.'
-        => 'The control panel requires JavaScript support. Here are the <a href="http://www.enable-javascript.com/" target="_blank">instructions how to enable JavaScript in your web browser</a>.',
+	'The control panel requires JavaScript support. Here are the <a href="http://www.enable-javascript.com/" target="_blank">instructions how to enable JavaScript in your web browser</a>.'
+		=> 'The control panel requires JavaScript support. Here are the <a href="http://www.enable-javascript.com/" target="_blank">instructions how to enable JavaScript in your web browser</a>.',
 
-    'This page does not exist.' => 'This page does not exist.',
+	'This page does not exist.' => 'This page does not exist.',
 
-    'Homepage' => 'Homepage',
+	'Homepage' => 'Homepage',
 
 );
