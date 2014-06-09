89df7d6850b18d9135b57f9ba839ae0c1771386f
Martin Hettiger <Martin@Hettiger.com>
2014-06-09 22:13:54 +0200

    Use the controller test case in the base controller tests

--- app/Larapress/Tests/Controllers/BaseControllerTest.php
+++ app/Larapress/Tests/Controllers/BaseControllerTest.php
@@ -1,50 +1,13 @@
 <?php namespace Larapress\Tests\Controllers;
 
-use Larapress\Tests\Controllers\Proxies\BaseControllerProxy;
-use Mockery;
-use Mockery\Mock;
-use PHPUnit_Framework_TestCase;
+use Larapress\Controllers\BaseController;
+use Larapress\Tests\Controllers\Templates\ControllerTestCase;
 
-class BaseControllerTest extends PHPUnit_Framework_TestCase {
-
-	/**
-	 * @var Mock
-	 */
-	private $helpers;
-
-	public function setUp()
-	{
-		parent::setUp();
-
-		$this->helpers = Mockery::mock('\Larapress\Interfaces\HelpersInterface');
-	}
-
-	public function tearDown()
-	{
-		parent::tearDown();
-
-		Mockery::close();
-	}
+class BaseControllerTest extends ControllerTestCase {
 
 	protected function getBaseControllerInstance()
 	{
-		return new BaseControllerProxy($this->helpers);
-	}
-
-	protected function getInitFixture()
-	{
-		$this->helpers->shouldReceive('initBaseController')->withNoArgs()->atLeast()->once();
-	}
-
-	/**
-	 * @test init() shares some important data for all larapress views
-	 */
-	public function init_shares_some_important_data_for_all_larapress_views()
-	{
-		$this->getInitFixture();
-		$controller = $this->getBaseControllerInstance();
-
-		$controller->init();
+		return new BaseController($this->helpers);
 	}
 
 	/**
@@ -52,7 +15,6 @@ class BaseControllerTest extends PHPUnit_Framework_TestCase {
 	 */
 	public function missingMethod_returns_the_force_404_helpers_method()
 	{
-		$this->getInitFixture();
 		$this->helpers->shouldReceive('force404')->withNoArgs()->once()->andReturn('baz');
 		$controller = $this->getBaseControllerInstance();
 
